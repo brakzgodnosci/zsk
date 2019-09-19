77521fec2d4c3a933862be642de35922b588583e
brakzgodnosci <54930869+brakzgodnosci@users.noreply.github.com>
2019-09-19 09:06:54 +0200

    Update zad2.php

--- zad/zad2.php
+++ zad/zad2.php
@@ -1,3 +1,10 @@
 <?php
-
+$potega =2**10;
+echo $potega,'<br><hr>';
+//
+//operatory bitowe: and &, or |, not ~~, xor ^,>>,<<
+$x = 0b1010;
+$x = $x >> 1; //101(2)=5
+$x = $x << 2; //10100(2)=20
+echo $x,'<br><hr>';
  ?>
